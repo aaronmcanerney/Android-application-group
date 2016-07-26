e2f02d34d7f8d1dd71f87ffaa10b30bb3084ae35
Roman Rogowski <rogowski@live.unc.edu>
2016-07-25 19:56:23 -0700

    Added functionality to push Events to server

--- app/src/main/java/com/example/aaron/fragmenttest/Event.java
+++ app/src/main/java/com/example/aaron/fragmenttest/Event.java
@@ -1,6 +1,8 @@
 package com.example.aaron.fragmenttest;
 
-import android.location.Location;
+import com.google.android.gms.location.places.Place;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -9,21 +11,55 @@ public class Event {
     private String creatorId;
     private String name;
     private String description;
-    private Location location;
-    private ArrayList<String> invitees; // ArrayList of uid's
+    private Place place;
+    private ArrayList<String> connections; // ArrayList of uid's
 
     public Event(String creatorId, String name, String description) {
         this.creatorId = creatorId;
         this.name = name;
         this.description = description;
+        this.connections = new ArrayList<>();
+
+        // Add creator as connection
+        addConnection(creatorId);
     }
 
-    public void setLocation(Location location) {
-        this.location = location;
+    public String getCreatorId() {
+        return creatorId;
     }
 
-    public Location getLocation() {
-        return this.location;
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void addConnection(String connectionId) {
+        connections.add(connectionId);
+    }
+
+    public void push() {
+        // Create event
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference events = database.child("events");
+        String eventId = events.push().getKey();
+        DatabaseReference event = events.child(eventId);
+        event.child("creatorId").setValue(creatorId);
+        event.child("name").setValue(name);
+        event.child("description").setValue(description);
+
+        // Push requests for event
+        DatabaseReference requests = database.child("requests");
+        for (String connectionId: connections) {
+            String response = (connectionId.equals(creatorId)) ? "accepted" : "pending";
+            requests.child(connectionId).child(eventId).setValue(response);
+        }
     }
 
 }
