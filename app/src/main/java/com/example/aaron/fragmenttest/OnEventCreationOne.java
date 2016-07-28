18a12cfce1c97ab4cb02b948c68563f23654c079
aaronmcanerney <aaron.mac016@yahoo.com>
2016-07-27 21:19:40 -0700

    Event Creation work, modified the look of the layout for setting event name and description, and resized the datepicker. Need help decided a color scheme, whenever i set a color it looks awful and cheesy.

--- app/src/main/java/com/example/aaron/fragmenttest/OnEventCreationOne.java
+++ app/src/main/java/com/example/aaron/fragmenttest/OnEventCreationOne.java
@@ -1,11 +1,22 @@
 package com.example.aaron.fragmenttest;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Typeface;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -32,13 +43,91 @@ public class OnEventCreationOne extends Fragment {
     }
     @Override
     public void onStart(){
+        //add a border to the relative layout
+        ShapeDrawable rectShapeDrawable = new ShapeDrawable();
+        Paint paint = rectShapeDrawable.getPaint();
+        paint.setColor(Color.GRAY);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+
+        //format the name again
+        SpannableString nameFormatted = new SpannableString("Event Name: ");
+        nameFormatted.setSpan(new UnderlineSpan(), 0, nameFormatted.length(), 0);
+        nameFormatted.setSpan(new StyleSpan(Typeface.BOLD), 0, nameFormatted.length(), 0);
+        nameFormatted.setSpan(new StyleSpan(Typeface.ITALIC), 0, nameFormatted.length(), 0);
+        //get the linear layout so everything is aligned vertically
+        RelativeLayout rl = (RelativeLayout) this.getActivity().findViewById(R.id.event_creation_one_linear_layout);
+        Display d = ((WindowManager)getActivity().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        Point point = getDisplaySize(d);
+        //create first rl
+        RelativeLayout enameRL = new RelativeLayout(this.getActivity());
+        rl.addView(enameRL);
+        //set the height and background
+        enameRL.getLayoutParams().height = point.y/5;
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) enameRL.getLayoutParams();
+        params.topMargin = point.x /15;
+        enameRL.setId(View.generateViewId());
+        enameRL.requestLayout();
+        enameRL.setBackground(rectShapeDrawable);
+        enameRL.setBackgroundColor(Color.BLUE);
+        //enameRL.setBackgroundResource(R.mipmap.back_layout);
+        //event name and edit text added to relative layout
         TextView ename = new TextView(this.getActivity());
-        ename.setText("Event Name: ");
+        ename.setText(nameFormatted);
         EditText editName = new EditText(this.getActivity());
+        enameRL.addView(ename);
+        enameRL.addView(editName);
+
+        //Create relative layout params for Event name
+        RelativeLayout.LayoutParams pname = (RelativeLayout.LayoutParams) ename.getLayoutParams();
+        pname.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        ename.setLayoutParams(pname);
+        ename.setId(View.generateViewId());
+        RelativeLayout.LayoutParams pEditName = (RelativeLayout.LayoutParams) editName.getLayoutParams();
+        pEditName.addRule(RelativeLayout.BELOW, ename.getId());
+        pEditName.width = point.x;
+        pEditName.height = point.y /5;
+        editName.setLayoutParams(pEditName);
+
+        //Creating Relative layout for description
+        /*RelativeLayout placeHolder = new RelativeLayout(this.getActivity());
+        rl.addView(placeHolder);
+        placeHolder.getLayoutParams().height = point.y/10;
+        RelativeLayout.LayoutParams placeParam = (RelativeLayout.LayoutParams) placeHolder.getLayoutParams();
+        placeParam.addRule(RelativeLayout.ALIGN_BASELINE);
+        */
+
+        RelativeLayout descriptionRL = new RelativeLayout(this.getActivity());
+        rl.addView(descriptionRL);
+        descriptionRL.setBackgroundColor(Color.BLUE);
+        descriptionRL.setBackground(rectShapeDrawable);
+        //descriptionRL.setBackgroundColor(Color.GRAY);
+        //set the height and background
+        descriptionRL.getLayoutParams().height = point.y/3;
         TextView eDescription = new TextView(this.getActivity());
         eDescription.setText("Description: ");
         EditText editDescription = new EditText(this.getActivity());
+        descriptionRL.addView(eDescription);
+        descriptionRL.addView(editDescription);
+        RelativeLayout.LayoutParams descriptionParams = (RelativeLayout.LayoutParams) eDescription.getLayoutParams();
+        pname.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        eDescription.setLayoutParams(descriptionParams);
+        eDescription.setId(View.generateViewId());
+        RelativeLayout.LayoutParams editDescriptionParams = (RelativeLayout.LayoutParams) editDescription.getLayoutParams();
+        editDescriptionParams.addRule(RelativeLayout.BELOW, eDescription.getId());
+        editDescriptionParams.width = point.x;
+        editDescriptionParams.height = point.y /3;
+        editDescription.setLayoutParams(editDescriptionParams);
+        enameRL.setId(View.generateViewId());
+        RelativeLayout.LayoutParams alignDescription = (RelativeLayout.LayoutParams) descriptionRL.getLayoutParams();
+       // alignDescription.addRule(RelativeLayout.ABOVE, enameRL.getId());
+        alignDescription.addRule(RelativeLayout.BELOW, enameRL.getId());
+        alignDescription.topMargin = point.y/10;
+        descriptionRL.setLayoutParams(alignDescription);
+
+
         Button button = new Button(this.getActivity());
+        rl.addView(button);
         button.setText("Next");
         button.setOnClickListener(new View.OnClickListener()
         {
@@ -49,12 +138,12 @@ public class OnEventCreationOne extends Fragment {
                 toEventCreationTwo(v);
             }
         });
-        RelativeLayout rl = (RelativeLayout) this.getActivity().findViewById(R.id.event_creation_one);
-        rl.addView(ename);
-        rl.addView(editName);
-        rl.addView(eDescription);
-        rl.addView(editDescription);
-        rl.addView(button);
+        RelativeLayout.LayoutParams buttonParams = (RelativeLayout.LayoutParams) button.getLayoutParams();
+        buttonParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        buttonParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        button.setLayoutParams(buttonParams);
+
+
 
         super.onStart();
     }
@@ -62,6 +151,18 @@ public class OnEventCreationOne extends Fragment {
         Fragment fragment = new OnEventCreationTwo();
         FragmentTransaction ft = this.getActivity().getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.event_container, fragment).addToBackStack(null).commit();
+    }
+    private static Point getDisplaySize(final Display display) {
+
+
+        final Point point = new Point();
+        try {
+            display.getSize(point);
+        } catch (java.lang.NoSuchMethodError ignore) { // Older device
+            point.x = display.getWidth();
+            point.y = display.getHeight();
+        }
+        return point;
     }
 
 }
--- app/src/main/java/com/example/aaron/fragmenttest/OnEventCreationTwo.java
+++ app/src/main/java/com/example/aaron/fragmenttest/OnEventCreationTwo.java
@@ -1,12 +1,17 @@
 package com.example.aaron.fragmenttest;
 
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.RelativeLayout;
 
 
@@ -28,6 +33,8 @@ public class OnEventCreationTwo extends Fragment {
         return f;
     }
     public void onStart(){
+        Display d = ((WindowManager)getActivity().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        Point point = getDisplaySize(d);
         Button button = new Button(this.getActivity());
         button.setText("Next");
         button.setOnClickListener(new View.OnClickListener()
@@ -40,7 +47,20 @@ public class OnEventCreationTwo extends Fragment {
             }
         });
         RelativeLayout rl = (RelativeLayout) this.getActivity().findViewById(R.id.event_creation_two);
+        DatePicker date = new DatePicker(this.getActivity());
+        rl.addView(date);
+
+        RelativeLayout.LayoutParams dateParams = (RelativeLayout.LayoutParams) date.getLayoutParams();
+        dateParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        date.getLayoutParams().height = point.y *3/4;
+        date.getLayoutParams().width = point.x;
+        //dateParams.topMargin = point.y/10;
+        date.setLayoutParams(dateParams);
         rl.addView(button);
+        RelativeLayout.LayoutParams buttonParams = (RelativeLayout.LayoutParams) button.getLayoutParams();
+        buttonParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        buttonParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+
 
         super.onStart();
     }
@@ -48,5 +68,17 @@ public class OnEventCreationTwo extends Fragment {
         Fragment fragment = new OnEventCreationThree();
         FragmentTransaction ft = this.getActivity().getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.event_container, fragment).addToBackStack(null).commit();
+    }
+    private static Point getDisplaySize(final Display display) {
+
+
+        final Point point = new Point();
+        try {
+            display.getSize(point);
+        } catch (java.lang.NoSuchMethodError ignore) { // Older device
+            point.x = display.getWidth();
+            point.y = display.getHeight();
+        }
+        return point;
     }
 }
